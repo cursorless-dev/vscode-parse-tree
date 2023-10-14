7edcf537fd71e154c453a4fe734508af93236a93
Aaron Adams <fidgetingbits@users.noreply.github.com>
2023-10-14 22:45:17 +0800

    Add tree-sitter-nix (#66)

--- src/extension.ts
+++ src/extension.ts
@@ -32,6 +32,7 @@ const languages: {
   kotlin: { module: "tree-sitter-kotlin" },
   latex: { module: "tree-sitter-latex" },
   markdown: { module: "tree-sitter-markdown" },
+  nix: { module: "tree-sitter-nix" },
   perl: { module: "tree-sitter-perl" },
   php: { module: "tree-sitter-php" },
   python: { module: "tree-sitter-python" },
