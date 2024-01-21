73acc96ab114bfe789eea4adec7373c041a26a50
fidgetingbits <fidgetingbits@memeoid.cx>
2024-01-21 14:52:50 +0800

    remove unfree

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; config.allowUnfree = true; };
+        pkgs = import nixpkgs { inherit overlays system; };
       });
     in
     {
